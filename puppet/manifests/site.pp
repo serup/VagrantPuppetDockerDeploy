3d1190df07d0acbf8bbe9527705f41babe57ff8a
christian <you@example.com>
2015-08-03 15:59:39 +0200

    Added changes for fetching locally stored docker image
    Change-Id: If9bc54196dc0d3b5e13ecf3464f599bc1bab5f68

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -30,15 +30,15 @@ node /^node01.*/ {
    image_tag => 'trusty',
   }
 
-  docker::run { 'helloworld':
-    image   => 'ubuntu',
-    command => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
-  }
-
-  docker::run { 'goodbyecruelworld':
-    image 	=> 'ubuntu',
-    command 	=> '/bin/sh -c "while true; do echo goodbye cruel world; sleep 1; done"',
-  }
+  #docker::run { 'helloworld':
+  #  image   => 'ubuntu',
+  #  command => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
+  #}
+#
+#  docker::run { 'goodbyecruelworld':
+#    image 	=> 'ubuntu',
+#    command 	=> '/bin/sh -c "while true; do echo goodbye cruel world; sleep 1; done"',
+#  }
 
 #  docker::image { 'whalesay': }
 
