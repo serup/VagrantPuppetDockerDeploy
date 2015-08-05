b44ea5147ef5f8b33e47fc5d280666f547b8305d
serup <johnny.serup@scanva.com>
2015-08-05 15:02:24 +0200

    docker run
    Change-Id: I93f3aac166448563be8b6ee6e53b96bad9494d9f

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -30,16 +30,13 @@ node /^node01.*/ {
    image_tag => 'trusty',
   }
 
-  #docker::run { 'helloworld':
-  #  image   => 'ubuntu',
-  #  command => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
-  #}
-#
-#  docker::run { 'goodbyecruelworld':
-#    image 	=> 'ubuntu',
-#    command 	=> '/bin/sh -c "while true; do echo goodbye cruel world; sleep 1; done"',
-#  }
+  docker::image { 'moderor':
+   docker_tar => '/vagrant/puppet/trunk/environments/devtest/modules/docker_images_download/docker-image-moderor.tar' 
+  }
 
-#  docker::image { 'whalesay': }
+  exec { "docker_run":
+     command => "/usr/bin/sudo docker run -d -t docker-image-moderor",
+     require => docker::image[moderor]
+  }
 
 }
